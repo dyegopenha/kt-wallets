ae3cd4278914b6232accf808f3560e872c88b556
Dyego Penha <dyegopenha@hotmail.com>
2025-11-08 10:59:39 -0300

    create kotlin project

---
+++ src/main/kotlin/com/example/kt_wallets/KtWalletsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kt_wallets
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtWalletsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KtWalletsApplication>(*args)
+}
